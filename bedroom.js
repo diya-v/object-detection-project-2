b9279f55ee5721b32f053e08c9c939da330f7df5
diya-v <96090673+diya-v@users.noreply.github.com>
2023-06-09 14:06:35 -0500

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,27 @@
+status="";
+
+function preload(){
+    img = loadImage("bedroom.png");
+}
+
+function setup(){
+    canvas = createCanvas(600,400);
+    canvas.center();
+    objectDetector=ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML="Detecting Object";
+}
+
+function modelLoaded(){
+    console.log("model loaded");
+    status= true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    else{
+    console.log(results);
+    }
+}
